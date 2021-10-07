84201b19a6a90f31bf61687bdb19f504b4ad1943
austinjp <austin.plunkett@gmail.com>
2021-10-07 13:46:31 +0100

    Make use of latest Swiper shared component

--- src/components/Swiper/index.js
+++ src/components/Swiper/index.js
@@ -1,64 +1,27 @@
 import { h, Component } from "preact"
 import { useContext } from "preact/compat"
 import { Theme } from "../../theme"
-import { View, Swiper as _Swiper } from "ui-shared/components"
+import { Swiper as _Swiper } from "ui-shared/components"
+import { mergeDeep } from "ui-shared/lib"
 
 import cxs from "cxs"
 
 const Swiper = (props) => {
   const theme = useContext(Theme)
-  const className = theme.swiper ? String(cxs({...theme.swiper})) : ""
-  const innerClassName = cxs({
-    height:"88%",
-    maxHeight:"88%",
-    width:"90%",
-    margin:"4% auto auto auto",
-    background:"yellow",
-    overflowX:"auto",
-    overflowY:"scroll"
-  })
+
+  const className =
+        cxs(mergeDeep({}, (theme.swiper || {}).frame || {}, props.style || {}))
+  const innerClassName =
+        cxs(mergeDeep({}, (theme.swiper || {}).inner || {}, props.style || {}))
+
+  const newProps = props
+  props.style && (newProps.style = undefined)
+
   return (
-    <_Swiper className={className} {...props}>
-      <View className={innerClassName}>
-        { /* props.children */ }
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-        <View>HI!</View>
-      </View>
+    <_Swiper className={className} {...newProps}>
+      <_Swiper.Inner className={innerClassName}>
+        {props.children}
+      </_Swiper.Inner>
     </_Swiper>
   )
 }
