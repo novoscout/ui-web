983abe6616f5663ff6e4ce5e1188a3b9112b6e87
austinjp <austin.plunkett@gmail.com>
2021-10-21 14:40:17 +0100

    Consistency
    and ensure inline styles are kept if they are used

--- src/components/Desk/index.js
+++ src/components/Desk/index.js
@@ -1,16 +1,18 @@
 import { h, Component } from "preact"
 import { useContext } from "preact/compat"
-import { Theme } from "../../theme"
+import cxs from "cxs"
+
 import { View } from "ui-shared/components"
 
-import cxs from "cxs"
+import { Theme } from "../../theme"
+
 
 // import quicklink from "quicklink/dist/quicklink.mjs"
 // quicklink()
 
 const Desk = (props) => {
   const theme = useContext(Theme)
-  const className = theme.desk ? String(cxs({...theme.desk})) : ""
+  const className = theme.desk ? String(cxs({...theme.desk})) : null
   return (
     <View className={className} {...props}>{props.children}</View>
   )
--- src/components/Swiper/index.js
+++ src/components/Swiper/index.js
@@ -1,24 +1,25 @@
 import { h, Component } from "preact"
 import { useContext } from "preact/compat"
-import { Theme } from "../../theme"
-import { Swiper as _Swiper } from "ui-shared/components"
-import { mergeDeep } from "ui-shared/lib"
-
 import cxs from "cxs"
+
+import { Swiper as _Swiper } from "ui-shared/components"
+
+import { Theme } from "../../theme"
+
 
 const Swiper = (props) => {
   const theme = useContext(Theme)
 
   const className =
-        cxs(mergeDeep({}, (theme.swiper || {}).frame || {}, props.style || {}))
+        (theme.swiper || {}).frame ? String(cxs(theme.swiper.frame)) : null
   const innerClassName =
-        cxs(mergeDeep({}, (theme.swiper || {}).inner || {}))
+        (theme.swiper || {}).inner ? String(cxs(theme.swiper.inner)) : null
 
-  const newProps = props
-  props.style && (newProps.style = undefined)
+  const newProps = {...props}
+  props.style && (newProps.style = null)
 
   return (
-    <_Swiper className={className} {...newProps}>
+    <_Swiper style={props.style} className={className} {...newProps}>
       <_Swiper.Inner className={innerClassName}>
         {props.children}
       </_Swiper.Inner>
