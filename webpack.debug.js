e948cb5e6b812e2034f1500aadf702b3986d5376
austinjp <austin.plunkett@gmail.com>
2021-11-06 15:10:38 +0000

    HTML and webpack config for debugging
    specifically at the moment for layout and swiping

--- webpack.debug.js
+++ webpack.debug.js
@@ -1,8 +1,17 @@
 const { merge } = require('webpack-merge');
+const htmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 const dev = require('./webpack.dev.js');
 
+dev.plugins = undefined;
+
 module.exports = merge(dev, {
-  entry: path.resolve(__dirname, "src/Debug/index.js")
+  entry: path.resolve(__dirname, "src/Debug/index.js"),
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './public/debug.html',
+      filename: 'index.html',
+    })
+  ]
 });
