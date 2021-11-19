475c16591ddff37128b8656b7aa6a44bd66b85d9
austinjp <austin.plunkett@gmail.com>
2021-11-19 10:07:56 +0000

    Oops add nav cancel component

---
+++ src/components/NavActionCancel/index.js
@@ -0,0 +1,41 @@
+import { h, Component } from "preact"
+import { useContext } from "preact/compat"
+import cxs from "cxs"
+
+import { NavAction } from "../NavAction"
+
+import { Theme } from "../../theme"
+
+
+class NavActionCancel extends Component {
+  constructor(props) {
+    super(props)
+    this.handleCancel = this.handleCancel.bind(this)
+  }
+
+  handleCancel() {
+    if (this.props.toggleModal) {
+      this.props.toggleModal({visible:false})
+    }
+  }
+  
+  render() {
+    const theme = useContext(Theme)
+    const className = String(cxs(theme.navActionCancel || {}))
+
+    return (
+      <NavAction
+        className={className}
+        {...this.props}>
+        <NavAction.Icon ariaLabel="Cancel">🗙</NavAction.Icon>
+        <NavAction.Text
+          href={null}
+          onclick={this.handleCancel}
+          >&nbsp;Cancel</NavAction.Text>
+      </NavAction>
+    )
+  }
+}
+
+export default NavActionCancel
+export { NavActionCancel }
