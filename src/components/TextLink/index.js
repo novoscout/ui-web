26adf4547124179d1f5a75919e066bc695548224
austinjp <austin.plunkett@gmail.com>
2021-12-03 08:19:00 +0000

    Removed old commented-out lines

--- src/components/TextLink/index.js
+++ src/components/TextLink/index.js
@@ -7,9 +7,6 @@ import { Link } from "preact-router/match"
 import { Theme } from "../../theme"
 
 
-// import quicklink from "quicklink/dist/quicklink.mjs"
-// quicklink()
-
 const TextLink = (props) => {
   const theme = useContext(Theme)
   const className = theme.textLink ? cxs(theme.textLink) : null
