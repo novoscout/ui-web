c586ade62c918f11d9e1596778da8d35a3d8941a
austinjp <austin.plunkett@gmail.com>
2022-01-06 23:06:09 +0000

    Possible issues with preact-router Link
    so using my own version, which can "force" page refresh by using location.replace() instead.

--- src/components/TextLink/index.js
+++ src/components/TextLink/index.js
@@ -1,18 +1,51 @@
 import { h, Component } from "preact"
 import { useContext } from "preact/compat"
+import { route } from "preact-router"
 import cxs from "cxs"
-
-import { Link } from "preact-router/match"
 
 import { Theme } from "../../theme"
 
 
-const TextLink = (props) => {
-  const theme = useContext(Theme)
-  const className = theme.textLink ? cxs(theme.textLink) : null
-  return (
-    <Link className={className} {...props}>{props.children}</Link>
-  )
+class TextLink extends Component {
+  handleOnClick(props,e) {
+    if (props.force && props.href) {
+      window.location.replace(props.href)
+      return
+    } else {
+      if (e) {
+        if (e.stopImmediatePropagation) { e.stopImmediatePropagation() }
+        if (e.stopPropagation) { e.stopPropagation() }
+        e.preventDefault()
+      }
+      if (props) {
+        if (props.onClick) { props.onClick() }
+        if (props.onclick) { props.onclick() }
+        if (props.href) {
+          if (props.replaceHistory) {
+            route(props.href, true)
+          } else {
+            route(props.href)
+          }
+        }
+      }
+    }
+  }
+
+  render() {
+    const theme = useContext(Theme)
+    const className = theme.textLink ? cxs(theme.textLink) : null
+    var newProps = {...this.props}
+    delete(newProps.href)
+    delete(newProps.onClick)
+    delete(newProps.onclick)
+    return (
+      <a
+        onClick={this.handleOnClick.bind(this,this.props)}
+        href={this.props.href}
+        className={className}
+        {...newProps}>{this.props.children}</a>
+    )
+  }
 }
 
 export default TextLink
