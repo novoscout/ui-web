cd19d722fce99a2da3a84f30d16ba988c8172a16
austinjp <austin.plunkett@gmail.com>
2022-01-23 12:52:04 +0000

    Monitor all files
    Inefficient but fine

--- monitor.sh
+++ monitor.sh
@@ -2,11 +2,11 @@
 
 # set -euo pipefail
 
-echo "Monitoring files in src..."
+echo "Monitoring files..."
 echo "Running '${@}' on changes."
 
 while true ; do (
-    inotifywait -r -e modify ./src/ | \
+    inotifywait -r -e modify . | \
         while read path _ file ; do
             echo "${path}${file}" modified!
             "${@}"
