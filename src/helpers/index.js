fecc4761ca2f694fb3245212caa58421d09d19e7
austinjp <austin.plunkett@gmail.com>
2022-02-25 14:14:46 +0000

    Import/export isOnline

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,12 +1,14 @@
 const shrinkTitle = require("./shrinkTitle")
 import { DOI } from "./doi"
 const getObjProp = require("./getObjProp")
+const isOnline = require("./isOnline")
 import { registerServiceWorker } from "./registerServiceWorker"
 const storage = require("./storage")
 
 export default {
   DOI,
   getObjProp,
+  isOnline,
   registerServiceWorker,
   shrinkTitle,
   storage,
@@ -15,6 +17,7 @@ export default {
 export {
   DOI,
   getObjProp,
+  isOnline,
   registerServiceWorker,
   shrinkTitle,
   storage,
